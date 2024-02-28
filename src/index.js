d3db71e29691c87bd84326a8fb13c000e8a559a3
Jack Holden <John_Holden@student.uml.edu>
2024-02-28 17:02:48 -0500

    remove app include in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 import Dashboard from './dashboard/dashboard'
@@ -9,7 +8,6 @@ import Dashboard from './dashboard/dashboard'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Dashboard />
   </React.StrictMode>,
 )
